8dfb65daa6ddc80164efa4404e2eeb37463b99ee
Jeffrey Szusz <jeff.szusz@gmail.com>
2015-08-22 01:16:06 -0400

    initial commit of tdd workshop code

---
+++ test.js
@@ -0,0 +1,17 @@
+describe('assert', function() {
+  it('works!', function(){
+    assert.equal(true, true);
+  });
+});
+
+describe('expect', function() {
+  it('works!', function(){
+    expect(true).to.be.true;
+  });
+});
+
+describe('should', function() {
+  it('works!', function(){
+    true.should.equal(true);
+  });
+});
